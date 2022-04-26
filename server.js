38eaa9547f017ab600b89e99a8f2600ac86a35ff
brooksat <brooksat@live.unc.edu>
2022-04-26 13:49:59 -0400

    rettesy

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ const logdb = require('./database.js')
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
-
+const cors = require('cors')
 // take an arbitrary port number as a command line argument 
 // Default: 5000
 const args = require('minimist')(process.argv.slice(2))
 
-const port = args.port || process.env.PORT || 5555
+const port = args.port || process.env.PORT || 5000
 
 // start app server
 const server = app.listen(port, () => {
@@ -45,11 +45,13 @@ if(args.help || args.h) {
   process.exit(0)
 }
 
-if(args.log != false) {
+if(args.log == 'false') {
+  console.log("dont create log")
+} else {
+
     const accesslog = fs.createWriteStream('access.log', { flags: 'a'})
     app.use(morgan('combined', {stream: accesslog}))
 }
-
 app.use((req, res, next) => {
     let logdata = {
       remoteaddr: req.ip,
@@ -69,6 +71,8 @@ app.use((req, res, next) => {
     
     next()
 })
+
+app.use(cors())
 
 if(args.debug) {
   app.get('app/log/access', (req, res) => {
@@ -152,3 +156,138 @@ app.get('/app/echo/:number',  (req, res) => {
     res.status(200).json({ 'message': req.params.number})
 })
 
+// /app/flip/ will be used to tesst single flip without import coin.mjs
+app.get('/app/flip/', (req, res) => {
+  const flip = coinFlip()
+  res.status(200).json({ 'flip': flip})
+})
+
+// /app/flips/:number is many flips 
+app.get('/app/flips/:number', (req, res) => {
+  const flips = coinFlips(req.params.number)
+  const tails = countTails(flips)
+  const heads = countHeads(flips)
+  res.status(200).json({'raw': flips, 'summary': {'heads': heads, 'tails': tails}})
+})
+
+// /app/flip/call/heads filp a coing with a call to heads
+app.get('/app/flip/call/heads', (req, res) => {
+  const str = 'heads'
+  const flip = coinFlip()
+  const result = win(flip, str)
+  res.status(200).json({ 'call': str, 'flip': flip, 'result': result})
+})
+
+// /app/flip/call/tails filp a coing with a call to tails
+app.get('/app/flip/call/tails', (req, res) => {
+const str = 'tails'
+const flip = coinFlip()
+const result = win(flip, str)
+res.status(200).json({ 'call': str, 'flip': flip, 'result': result})
+})
+
+//Define default endpoint
+//default response for any other request
+app.use(function(req, res) {
+  res.status(404).send('404 NOT FOUND')
+})
+
+
+
+// Functions
+
+function coinFlip() {
+  let x = Math.random()
+  if(x >= 0.5) {
+    return 'heads'
+  } else {
+    return 'tails'
+  }
+}
+
+function coinFlips(number) {
+  var tosses = []
+  for (var i = 0; i < number; i++) {
+    tosses.push(coinFlip())
+  }
+  return tosses
+}
+
+function countFlips(array) {
+  let count_tails = 0
+  let count_heads = 0
+  for(let i = 0; i < array.length; i++) {
+    if(array[i] == 'tails') {
+      count_tails++;
+    } else if(array[i] == 'heads') {
+      count_heads++;
+    }
+  }
+
+  if(count_tails == 0) {
+    return('{ heads: ' + count_heads + ' }')
+  } else if (count_heads == 0) {
+    return('{ tails: ' + count_tails + ' }')
+  } else {
+  return('{ heads: ' + count_heads + ', tails: ' + count_tails + ' }')
+  }
+}
+
+/** Flip a coin!
+ * 
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
+ * 
+ * @param {string} call 
+ * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
+ * 
+ * example: flipACoin('tails')
+ * returns: { call: 'tails', flip: 'heads', result: 'lose' }
+ */
+
+function flipACoin(call) {
+  if(call == null) {
+    return('Error: no input') //return error no input
+  }
+  if(call == 'heads' || call == 'tails') {
+    let flip = coinFlip()
+    let result = 'lose'
+    if(flip == call) {
+      result = 'win'
+    }
+    return("{ call: '" + call + "', flip: '" + flip + "', result: '" + result + "' }")
+  } else {
+    return('Usage: node guess-flip.js --call= [heads | tails]')
+
+  }
+  
+}
+
+function win(flip, call) {
+  let result = 'lose'
+  if(flip == call) {
+    result = 'win'
+  }
+  return(result)
+}
+
+function countTails(flips) {
+  let countT = 0;
+  for(var i = 0; i < flips.length; i++) {
+    if(flips[i] == 'tails') {
+      countT++;
+    }
+  }
+
+  return countT
+}
+
+function countHeads(flips) {
+  let countH = 0;
+  for(var i = 0; i < flips.length; i++) {
+    if(flips[i] == 'heads') {
+      countH++;
+    }
+  }
+
+  return countH
+}
